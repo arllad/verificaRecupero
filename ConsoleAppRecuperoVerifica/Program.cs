6916c38b5071362cd1444d53cc96d58feaf35679
Olmo Dallara <OLMO.DALLARA@local.ittcesena.it>
2023-03-18 09:21:50 +0100

    alunno 3: classe e funzioni

--- ConsoleAppRecuperoVerifica/Program.cs
+++ ConsoleAppRecuperoVerifica/Program.cs
@@ -8,6 +8,30 @@ namespace ConsoleAppRecuperoVerifica
 {
     internal class Program
     {
+
+        class alunno
+        {
+            string nome;
+            string cognome;
+            int età;
+        }
+
+        string classi(int età)
+        {
+            if (età < 16)
+            {
+                return "biennio";
+            }
+            else
+            {
+                return "triennio";
+            }
+        }
+
+        string toString(string nome, string cognome, int età)
+        {
+            return nome + cognome + età;
+        }
         static void Main(string[] args)
         {
 
